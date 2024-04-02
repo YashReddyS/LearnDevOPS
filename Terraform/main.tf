a76446a4726e38a2c4af765664a0525ff48a41c6
yashwanth reddy <syashreddys@gmail.com>
2024-04-01 22:50:31 -0700

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
-  location = "us-central1"
+  location = "us-central1-f"
 
   remove_default_node_pool = true
   initial_node_count       = 1
